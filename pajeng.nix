e310f4ad213df6a293c33b01476b49dcc3e5624a
sacolle <pedro.h.b.colle@gmail.com>
2026-03-12 09:24:49 -0300

    adicionado modificações nas opções de compilação do pajeng

--- pajeng.nix
+++ pajeng.nix
@@ -9,50 +9,52 @@
     asciidoc,
     flex,
     bison,
-    fmt,
-
-    #debug
-    patchelf
-
+    fmt
 }:
-stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: 
+let 
+    cmakeOpt = name: val: "D${name}=${toString val}";
+in
+{
     pname = "pajeng";
     system = "x86_64-linux";
     version = "1.3.10";
 
-    #src = ./.;
+    src = ./.;
 
-   src = fetchFromGitHub {
-       owner = "schnorr";
-       repo = "pajeng";
-       rev =  "2926ab4208eb5d8b11f8fa33f35c2d40c86480f9";# finalAttrs.version;
-       hash = "sha256-g3aT5SNwrhk7d/f5ElJfSqXQ6MFnsuVQ0fSpbiH94Y0=";
-   };
+  #src = fetchFromGitHub {
+  #    owner = "schnorr";
+  #    repo = "pajeng";
+  #    rev =  "2926ab4208eb5d8b11f8fa33f35c2d40c86480f9";# finalAttrs.version;
+  #    hash = "sha256-g3aT5SNwrhk7d/f5ElJfSqXQ6MFnsuVQ0fSpbiH94Y0=";
+  #};
     nativeBuildInputs = [
         perl
-	cmake # adding cmake as a buildinput does everything else automatically
-	
-	boost
-	asciidoc
-	flex
-	bison
-	fmt
-
-	patchelf
+        cmake # adding cmake as a buildinput does everything else automatically
+        
+        boost
+        asciidoc
+        flex
+        bison
+        fmt
     ];
 
-    buildInputs = [ 
-    ];
+    buildInputs = [ boost fmt ];
     # poti's cmake version is less then 3.5, so i need to tell it do it anyway.
     cmakeFlags = [ 
     	# "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" 
-	# Tells CMake to use the final installation RPATH even during the build phase
-	"-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
-	# Prevents CMake from adding the build directory to the RPATH
-	"-DCMAKE_SKIP_BUILD_RPATH=OFF" 
-	# Ensure the install RPATH is actually set to the Nix store lib path
-	"-DCMAKE_INSTALL_RPATH=${placeholder "out"}/lib"
-	"-DCMAKE_BUILD_TYPE=Release"
+        # Tells CMake to use the final installation RPATH even during the build phase
+        "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
+        # Prevents CMake from adding the build directory to the RPATH
+        "-DCMAKE_SKIP_BUILD_RPATH=OFF" 
+        # Ensure the install RPATH is actually set to the Nix store lib path
+        "-DCMAKE_INSTALL_RPATH=${placeholder "out"}/lib"
+        "-DCMAKE_BUILD_TYPE=Release"
+        (cmakeOpt "PAJE_DOC" "OFF")
+        (cmakeOpt "PAJE_LIBRARY" "ON")
+        (cmakeOpt "PAJE_TOOLS" "ON")
+        (cmakeOpt "PAJE_USE_FMT" "ON")
+        (cmakeOpt "STATIC_LINKING" "OFF")
     ];
 
     # esse aqui foi dica do amiguinho
@@ -62,11 +64,7 @@ stdenv.mkDerivation (finalAttrs: {
     # então fazemos essa inserção manual para que os testes funcionem
     # TODO: validar que deu certo
     preCheck = ''
-	export LD_LIBRARY_PATH=$PWD/src/libpaje:$LD_LIBRARY_PATH
-    '';
-
-    postInstall = ''
-	patchelf --print-rpath "$out/bin/pj_dump"
+        export LD_LIBRARY_PATH=$PWD/src/libpaje:$LD_LIBRARY_PATH
     '';
 
     doCheck = true;
